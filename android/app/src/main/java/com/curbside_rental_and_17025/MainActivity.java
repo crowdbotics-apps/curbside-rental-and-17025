89532406e5d9dc911ca0d5054ac225f908a2da28
Crowdbotics <team@crowdbotics.com>
2020-05-14 22:09:51 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/curbside_rental_and_17025/MainActivity.java
@@ -0,0 +1,15 @@
+package com.curbside_rental_and_17025;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "curbside_rental_and_17025";
+    }
+}
